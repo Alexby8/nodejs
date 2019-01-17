cf8bc021bb38ee43560bfba9d9231e45b4ed22a0
Alexey <alexby8@yandex.ru>
2019-01-17 11:18:26 +0300

    New updates

--- lesson.js
+++ lesson.js
@@ -1,4 +1,14 @@
 const ansi = require("ansi");
 const cursor = ansi(process.stdout);
 
-cursor.write("Hello!");
+//cursor.write("Hello!");
+
+var program = require('commander');
+ 
+program
+  .option('--no-sauce', 'Remove sauce')
+  .parse(process.argv);
+ 
+console.log('you ordered a pizza');
+if (program.sauce) console.log('  with sauce');
+else console.log(' without sauce');
